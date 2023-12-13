d9d64e1069330a49cb1d14570b33288b8f88aa96
Yorcal <43748206+Yorcal@users.noreply.github.com>
2023-12-13 12:16:57 +0100

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+export const publicToken = "public_9zo3ka9T1MiEBfhE";
+export const mainSceneUUID = "a733f676-ce54-40ec-9e30-1a1746002749";
+export const characterControllerSceneUUID = "3e00b0a9-b4ad-4ae2-b2af-7bf2439aa05a";
+export const cubeSceneUUID = "9efec576-c61d-4606-bdd8-9461274a2b21";
